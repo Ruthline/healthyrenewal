d8a43a77d75f997f987f22b44d5e5c7cb5fc3a14
Ruthline <rutheveline@hotmail.es>
2023-09-03 18:49:40 -0500

    Structure folders with react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Navigate,Route, Routes} from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/home/Home';
+import AboutUs from './pages/aboutUs/AboutUs';
+import SingIn from './pages/signin/SignIn';
+import SignUp from './pages/signup/SignUp';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/hr-home/' element={<Home/>}></Route>
+    <Route path='/home' element={<Navigate replace to={"/steam-home/"}/>}></Route>
+    <Route path='/aboutUs' element={<AboutUs/>}></Route>
+    <Route path='/signIn' element={<SingIn/>}></Route>
+    <Route path='/singUp' element={<SignUp/>}></Route>
+  </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/signin/SignIn.js
@@ -0,0 +1,8 @@
+function SingIn(){
+return(
+    <div></div>
+);
+
+};
+
+export default SingIn;
